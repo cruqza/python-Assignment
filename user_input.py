8e47af6cd68932df74b69a83dd05cb13d1ccb7c8
cruqza <mugendierickson@gmail.com>
2024-08-21 19:17:40 +0300

    Assignment 1 python

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("please enter your name: ")
+age = int(input("please enter your age?: "))
+location = input("please enter where you live: ")
+print(f"hello {name}, you are {age} years old and you live in {location}.")
